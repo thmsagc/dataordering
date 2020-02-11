9f3481f8aa9f830254b381d653600c7267a9ccaf
thmsagc <52658569+thmsagc@users.noreply.github.com>
2020-02-11 12:08:56 -0300

    Add files via upload

--- ALGORITMO DE DIJKSTRA/source/dijkstra.cpp
+++ ALGORITMO DE DIJKSTRA/source/dijkstra.cpp
@@ -1,127 +1,128 @@
-/*
-	 ALGORITMOS DE ORDENAÇÃO
-PROBLEMA DO CAMINHO MÍNIMO (DIJKSTRA)
-
-	@author Thomás Augusto Gouveia Chaves
-	@author 20180002671
-*/
-
 #include "dijkstra.h"
 
+int main(int argc, char **argv) {
+	if(argc != 3){
+	    puts("Inicie com: nome_da_instancia vertice_de_origem (ex. dij10.txt 1).");
+	    exit(1);
+	}
 
-#include <string>
-#include <fstream>
-#include <iostream>
-#include <limits>
-#include <vector>
+	string dir = argv[1];
+	int s = atoi(argv[2]);
+
+	Grafo g(dir);
+
+	vector<int> p(g.getNumVertices());
+	vector<int> d = initialize_single_source(g.getNumVertices(), s, p);
+
+	dijkstra(g, d, p);
+
+    ofstream result("resultado.txt");
+
+    if(!result){
+    	puts("Erro ao criar arquivo de saída!");
+    	exit(0);
+    }
+
+    for(int i = 0; i < d.size(); i++){
+    	result << s <<" a " << i << ": "  << d[i] << " ";
+    	result << endl;
+    }
+
+    result.close();
+}
+
+void dijkstra(Grafo g, vector<int> &d, vector<int> &p){
+
+	//vértices que foram visitados
+    vector<bool> S(g.getNumVertices());
+    //fila de prioridade
+    vector<pair<int, int>> fila;
+    for(int i = 0; i < g.getNumVertices(); i++)
+    	fp_minInsert(fila, i, d[i]);
 
 
-int main(){
+    while(!fila.empty()){
+	    pair<int, int> u = fp_extractMin(fila);
+	    S[u.first] = true;
+	    for(int id_v = 0; id_v < fila.size(); id_v++)
+	        relax(u, id_v, g, fila, d, p);
+    }
+}
 
-    puts("Insira o local do arquivo:");
-    cin.ignore();
-	string dir;
-	getline(cin, dir);
+vector<int> initialize_single_source(int vertices, int v_origem, vector<int> &p){
+	vector<int> d(vertices);
+	for(int i = 0; i < d.size(); i++){
+		d[i] = infinito;
+		p[i] = -1;
+	}
+	d[v_origem] = 0;
+	return d;
+}
 
-	ifstream arq(dir, ios::in);
+void relax(pair<int, int> u, int id_v, Grafo g, vector<pair<int, int>> &fila, vector<int> &d, vector<int> &p){
+	pair<int, int> v = fila[id_v];
+    int w = g.getMatriz()[u.first][v.first];
+    if(v.second > u.second + w){
+    	d[v.first] = u.second + w;
 
-    if (!arq){
-        puts("Erro ao abrir o arquivo!");
+    	fp_decreaseKey(fila, (w + u.second), id_v);
+    	p[id_v] = u.first;
+    }
+}
+
+/* FILA DE PRIORIDADE */
+
+void fp_decreaseKey(vector<pair<int, int>> &fp, int chave, int i){
+    if(chave < fp[i].second){
+        fp[i].second = chave;
+
+        if(fp.size() == 1) return;
+
+        int pai = (i-1)/2;
+        while (fp[pai].second > fp[i].second){
+            swap(fp[i], fp[pai]);
+            i = pai;
+            pai = (i-1)/2;
+        }
+    }
+}
+
+pair<int, int> fp_extractMin(vector<pair<int, int>> &fp){
+    if(fp.size() < 1){
+        puts("heap underflow");
         exit(1);
     }
 
-    Grafo grafo;
+    pair<int, int> menor = fp[0];
+    swap(fp[0], fp[fp.size()-1]);
+    fp.pop_back();
+    fp_minHeapify(fp, 0, fp.size());
+    return menor;
+}
 
-    int vertices;
-    arq >> vertices;
-    vector<vector<int>> mat(vertices, vector<int>(vertices));
+void fp_minHeapify(vector<pair<int, int>> &fp, int i, int ultimo){
+    int menor = i;
+    int fesq = i*2+1;
+    int fdir = i*2+2;
 
-    for(int row = 0; row < vertices; row++){
-        for(int col = row; col < vertices; col++){
-            if(row == col)
-                mat[row][col] = 0;
-            else
-                arq >> mat[row][col];
-
-            mat[col][row] = mat[row][col];
+    if(fesq < ultimo){
+        if(fp[fesq].second < fp[menor].second){
+            menor = fesq;
         }
     }
-
-    grafo.matriz = mat;
-    grafo.num_vertices = vertices;
-
-    puts("Insira o indice desejado do grafo:");
-    cin.ignore();
-	int u;
-	scanf("%d", &u);
-
-	dijkstra(grafo, u);
+    if(fdir < ultimo){
+        if(fp[fdir].second < fp[menor].second){
+            menor = fdir;
+        }
+    }
+    if(menor != i){
+        swap(fp[i], fp[menor]);
+        fp_minHeapify(fp, menor, ultimo);
+    }
 }
 
-void dijkstra(Grafo grafo, int u) {
-	/* Inicilização do grafo */
-	vector<int> pi(grafo.num_vertices);
-	vector<int> d = initialize_single_source(grafo.num_vertices, pi, u);
-	vector<bool> s(grafo.num_vertices);
-	/**/
-
-	/* Criação e inicialização da fila de prioridade mínima */
-	vector<pair<int, int>> fila_pri_min;
-
-	for(int i = 0; i < grafo.num_vertices; i++)
-		heap_min_insert(fila_pri_min, i, d[i]);
-	/**/
-
-	/* Dijkstra */
-	while (!fila_pri_min.empty()) {
-		pair<int, int> u = heap_extract_min(fila_pri_min);
-		s[u.first] = true;
-		for(int i = 0; i < fila_pri_min.size(); i++){
-			relax(u, i, grafo, fila_pri_min, d, pi);
-		}
-	}
-	/**/
-	gerar_resultado(u, d);
+void fp_minInsert(vector<pair<int, int>> &fp, int i, int chave){
+    pair<int, int> p = {i, infinito};
+    fp.push_back(p);
+    fp_decreaseKey(fp, chave, i);
 }
-
-void gerar_resultado(int u, vector<int> d){
-	ofstream arq("result.txt");
-
-	if(!arq){
-		puts("Erro ao criar o arquivo de saída!");
-		exit(1);
-	}
-
-	for(int i = 0; i < d.size(); i++) {
-		arq << u <<"→" << i << ": "  << d[i] << " ";
-		arq << endl;
-	}
-
-	arq.close();
-}
-
-vector<int> initialize_single_source(int tamanho, vector<int> &pi, int u){
-	
-	vector<int> d(tamanho);
-
-	for(int i = 0; i < d.size(); i++){
-	    d[i] = numeric_limits<int>::max(); 	  //Inicializa o vértice com valor infinito
-	    pi[i] = -1;							  //Predecessores inicialmente com valor -1
-	}
-
-	d[u] = 0;						  	  	  //Valor do índice inicialmente igual a 0
-	return d;							  	  //Vetor de distâncias inicializado
-}
-
-void relax(pair<int, int> u, int v, Grafo grafo, vector<pair<int, int>> &fila_pri_min, vector<int> &d, vector<int> &p){
-	int inc_aresta = grafo.matriz[u.first][fila_pri_min[v].first];
-	int w = u.second + inc_aresta;
-
-	if(fila_pri_min[v].second > w){
-	    d[fila_pri_min[v].first] = w;
-	    heap_decrease_key(fila_pri_min, w, v);
-	    p[v] = u.first;
-	}
-}
-
-
--- ALGORITMO DE DIJKSTRA/source/dijkstra.h
+++ ALGORITMO DE DIJKSTRA/source/dijkstra.h
@@ -1,31 +1,84 @@
-/*
-	 ALGORITMOS DE ORDENAÇÃO
-PROBLEMA DO CAMINHO MÍNIMO (DIJKSTRA)
-
-	@author Thomás Augusto Gouveia Chaves
-	@author 20180002671
-*/
-
-
 #ifndef DIJKSTRA_H
 #define DIJKSTRA_H
 
-#include "fila_de_prioridade.h"
+#include <iostream>
 #include <vector>
+#include <limits>
+#include <fstream>
 
 using namespace std;
+#define infinito numeric_limits<int>::max()
 
+class Grafo{
+private:
+	void ler_arquivo(string dir);
+	vector<vector<int>> matriz_de_adjacencias;
+	int num_vertices;
 
-struct grafo{
-	vector<vector<int>> matriz;
-    int num_vertices;
+public:
+	Grafo(string dir){
+		ler_arquivo(dir);
+	}
+
+	~Grafo(){
+
+	}
+
+	vector<vector<int>> getMatriz(){
+		return matriz_de_adjacencias;
+	}
+
+	int getNumVertices(){
+		return num_vertices;
+	}
+
+	void setMatriz(vector<vector<int>> m){
+		matriz_de_adjacencias = m;
+	}
+
+	void setNumVertices(int n){
+		num_vertices = n;
+	}
 };
-typedef struct grafo Grafo;
 
-void dijkstra(Grafo grafo, int u);
-void gerar_resultado(int u, vector<int> d);
-vector<int> initialize_single_source(int tamanho, vector<int> &p, int u);
-void relax(pair<int, int> u, int v, Grafo grafo, vector<pair<int, int>> &h, vector<int> &d, vector<int> &p);
+void Grafo::ler_arquivo(string dir){
+	ifstream instancia(dir, ios::in);
+
+	if(!instancia){
+		puts("Erro ao abrir instância numérica!");
+		exit(1);
+	}
+
+	int vertices;
+	instancia >> vertices;
+
+	vector<vector<int>> matriz(vertices, vector<int>(vertices));
+
+	for(int i = 0; i < vertices; i++)
+		for(int j = i; j < vertices; j++){
+			if(i == j)
+				matriz[i][j] = 0;
+			else
+				instancia >> matriz[i][j];
+
+			matriz[j][i] = matriz[i][j];
+		}
+
+	setMatriz(matriz);
+	setNumVertices(vertices);
+	
+	instancia.close();
+}
+
+
+vector<int> initialize_single_source(int vertices, int v_origem, vector<int> &p);
+void dijkstra(Grafo g, vector<int> &d, vector<int> &p);
+void relax(pair<int, int> noAtual, int vizinho, Grafo data, vector<pair<int, int>> &heap, vector<int> &dist, vector<int> &p);
+
+void fp_decreaseKey(vector<pair<int, int>> &fp, int valor, int i);
+pair<int, int> fp_extractMin(vector<pair<int, int>> &fp);
+void fp_minHeapify(vector<pair<int, int>> &fp, int no, int fim);
+void fp_minInsert(vector<pair<int, int>> &fp, int i, int valor);
 
 
 #endif
