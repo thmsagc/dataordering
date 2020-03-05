042138c653bb14cd0e525668ad30061569669d54
thmsagc <52658569+thmsagc@users.noreply.github.com>
2020-03-04 22:28:52 -0300

    Update mergesort.cpp

--- MERGE SORT/source/mergesort.cpp
+++ MERGE SORT/source/mergesort.cpp
@@ -26,8 +26,7 @@ int main(int argc, char **argv){
 	for(int i = 0; i < elementos; i++){
 		instancia >> vetor[i];
 	}
-
-	vetor = merge_sort(vetor);
+	merge_sort(vetor, 0, elementos-1);
 
 	ofstream resultado("resultado.txt");
 	if(!resultado){
@@ -42,42 +41,54 @@ int main(int argc, char **argv){
 	return 0;
 }
 
+void intercalacao(vector<int> &v, int inicio, int meio, int fim) 
+{ 
+    int i, j, k; 
+    int p1 = meio - inicio + 1; 
+    int p2 =  fim - meio; 
+  
+    int esquerda[p1], direita[p2]; 
+  
+    for (i = 0; i < p1; i++) 
+        esquerda[i] = v[inicio + i];
+    
+    for (j = 0; j < p2; j++) 
+        direita[j] = v[meio + 1+ j]; 
+ 
+    i = 0;
+    j = 0;
+    k = inicio;
 
-vector<int> merge_sort(vector<int> &v, int inicio, int tamanho){
-	int p = tamanho/2;
-	if(p < 1)
-		return;
-	merge_sort(v, inicio, p);
-	merge_sort(v, p+1, tamanho);
-	intercalacao(v, inicio, meio, tamanho);
-}
+    while (i < p1 && j < p2){ 
+        if (esquerda[i] <= direita[j]){ 
+            v[k] = esquerda[i]; 
+            i++; 
+        } else { 
+            v[k] = direita[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+  
+    while (i < p1){ 
+        v[k] = esquerda[i]; 
+        i++; 
+        k++; 
+    } 
 
-void intercalacao(vector<int> &v, int inicio, int meio, int fim){
-	int i = inicio;
-	int j = meio + 1;
-	int k = inicio;
-	vector<int> aux(v.size()) = v;
+    while (j < p2){ 
+        v[k] = direita[j]; 
+        j++; 
+        k++; 
+    } 
+} 
 
-	while(i <= meio && j <= fim){
-		if(aux[i] < aux[j]){
-			v[k] = aux[i];
-			i++;		
-		} else {
-			v[k] = aux[j];
-			j++;
-		}
-		k++;
-	}
-
-	for(i; i <= meio; i++){
-		v[k] = aux[i];
-		k++;
-	}
-
-	for(j; j <= fim; j++){
-		v[k] = aux[j];	
-		j++;
-		k++;
-	}
-	return;
-}
+void merge_sort(vector<int> &v, int inicio, int fim) 
+{ 
+    if (inicio < fim){ 
+        int meio = inicio+(fim-inicio)/2; 
+        merge_sort(v, inicio, meio); 
+        merge_sort(v, meio+1, fim); 
+        intercalacao(v, inicio, meio, fim); 
+    } 
+} 
